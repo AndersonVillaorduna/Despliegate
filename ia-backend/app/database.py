6526579e8132fb6d4b675b0ee5a3403a222d3bc8
Anderson Villaorduña <vvillaordunasil@ucvvirtual.edu.pe>
2025-06-25 22:23:40 -0500

    feat:NuevaDataBase

--- ia-backend/app/database.py
+++ ia-backend/app/database.py
@@ -1,15 +1,17 @@
-import mysql.connector
-from mysql.connector import Error
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 def conectar():
     try:
-        conexion = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='',
-            database='ucvbot'
-        )
+        urldatabase = os.getenv('urldatabase')
+        if not urldatabase:
+            raise ValueError("La variable de entorno 'urldatabase' no está configurada.")
+
+        conexion = psycopg2.connect(urldatabase)
         return conexion
-    except Error as e:
-        print(f"Error al conectar: {e}")
+    except Exception as e:
+        print(f"Error al conectar a la base de datos PostgreSQL: {e}")
         return None
